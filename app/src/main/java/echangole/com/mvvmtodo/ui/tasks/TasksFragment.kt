c91a232e65fa20bb0599dc2f88c060754ed0f412
manu <manuchangole@gmail.com>
2021-02-14 22:22:47 +0300

    RecyclerView and ViewModel

--- app/src/main/java/echangole/com/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/echangole/com/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -1,14 +1,36 @@
 package echangole.com.mvvmtodo.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import echangole.com.mvvmtodo.R
+import echangole.com.mvvmtodo.databinding.FragmentTasksBinding
 
 @AndroidEntryPoint
 class TasksFragment:Fragment(R.layout.fragment_tasks )
 {
 
     private val viewModel:TaskViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding=FragmentTasksBinding.bind(view)
+        val taskAdapter=TaskAdapter()
+        binding.apply {
+            taskRecyclerView.apply{
+                adapter=taskAdapter
+                layoutManager=LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            } }
+
+       viewModel.task.observe(viewLifecycleOwner)
+       {
+           taskAdapter.submitList(it)
+       }
+    }
 }
